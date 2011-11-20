ab935d75a34f6984da2b8840e87a7a03b558148e
Gerard O'Neill <grardb@pastina.rutgers.edu>
2011-11-20 00:19:49 -0500

    We now have planet functionality and tracking of elapsed time between loops

--- Game.cpp
+++ Game.cpp
@@ -13,29 +13,60 @@ using namespace std;
 int main() {
 	// Create main window
 	sf::RenderWindow App(sf::VideoMode(800, 600), "SFML Shapes");
+	
+	// Clock to keep track of time between game loop iterations
+	// Used to keep the game running smoothly
+	sf::Clock Clock;
+
+	// Color of the main planet
+	sf::Color mainColor(100, 100, 100);
+
+	// List of planets 
+	vector<Planet::Planet> Planets;
+
+	// The center of the window
+	int WindowMidX = App.GetWidth()/2;
+	int WindowMidY = App.getHeight()/2;
+
+	// Add the main planet (being controlled by the gamer) to the center
+	// of the window and then add it to the list of planets.
+	Planet::Planet mainPlanet(WindowMidX, WindowMidY, STARTING_PLANET_SIZE, mainColor);
+	Planets.push_back(mainPlanet);
 
 	// Start game loop
 	while (App.IsOpened()) {
+
+		// Get the time elapsed since the last 
+		float Time = Clock.GetElapsedTime();
+		Clock.Reset();
+
 		// Process events
 		sf::Event Event;
 		while (App.GetEvent(Event)) {
 
-			//Close window : exit
+			// Close window : exit
 			if (Event.Type == sf::Event::Closed) {
 				App.Close();
 			}
+
+			// Clear screen
+			App.Clear();
 			
-			//Mouse button
+			// Track mouse clicks
 			if(Event.Type == sf::Event::MouseButtonReleased) {
 				cout << "X = " << Event.MouseButton.X << " Y = " << Event.MouseButton.Y << endl;
 			}
 		}
 
-		// Clear screen
-		App.Clear();
+		// Redraw all the circles
+		for (int i = Planets.begin(); i != Planet.end(); i++) {
 
-		//draw the circle
-		App.Draw(sf::Shape::Circle(200, 200, 100, sf::Color::Yellow));
+			// Current planet
+			Planet CurrentPlanet = Planets[i];
+
+			// Draw the planet
+			App.Draw(CurrentPlanet.GetShape());
+		}
 
 		// Finally, display the rendered frame on screen
 		App.Display();
